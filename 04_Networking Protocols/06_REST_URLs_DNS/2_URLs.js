7e9aef7c01a367fa5463fbd3ae2941fb69dd7dd6
heshamalsaqqaf2 <heshamdev2@gmail.com>
2025-03-22 01:31:14 +0300

    URLs.

---
+++ 04_Networking Protocols/06_REST_URLs_DNS/2_URLs.js
@@ -0,0 +1,18 @@
+/**
+ * @description
+ *!  URLs (Uniform Resource Locators)
+ *   A URL is the address used to access resources on the web. It consists of several parts: the
+ *   protocol (HTTP or HTTPS), the domain name, and optionally the port and path. URLs enable
+ *   clients to locate and request specific resources from a server.
+ * 
+ *   In NodeJS, working with URLs is essential for handling and routing requests. The url module
+ *   allows you to parse and manipulate URLs to extract relevant information.
+*/
+
+const url = require('url');
+let urlPath = new URL('https://example.com:8000/path?name=alice');
+
+console.log(urlPath.hostname);
+console.log(urlPath.port);
+console.log(urlPath.pathname);
+console.log(urlPath.searchParams.get('name'));
