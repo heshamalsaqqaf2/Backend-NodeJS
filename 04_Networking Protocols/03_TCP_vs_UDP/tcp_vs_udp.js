0382a9525cc7ee547a70cb00929c4c790b39cd41
heshamalsaqqaf2 <heshamdev2@gmail.com>
2025-03-22 00:08:20 +0300

    TCP VS UDP.

--- 04_Networking Protocols/03_TCP_vs_UDP/tcp_vs_udp.js
+++ 04_Networking Protocols/03_TCP_vs_UDP/tcp_vs_udp.js
@@ -1,15 +1,17 @@
 /** 
- ** @description
- *!  TCP
- *    TCP (Transmission Control Protocol) is a connection-oriented protocol that ensures data is
+ * @description
+ *?  TCP
+ **   TCP (Transmission Control Protocol) 
+ *    is a connection-oriented protocol that ensures data is
  *    transmitted reliably. It establishes a connection between the sender and receiver before any data
  *    is transferred and guarantees that all packets arrive in order and without errors.
  *    This makes TCP ideal for applications where data integrity is essential, such as web servers and databases.
- *?    For instance, a NodeJS server handling a large number of client requests relies on TCP
+ *!   For instance, a NodeJS server handling a large number of client requests relies on TCP
  *    to ensure that each request is accurately received and processed in the correct sequence.
  * 
- *!  UDP
- *    In contrast, UDP (User Datagram Protocol) is connectionless and does not provide guarantees
+ *?  UDP
+ **   UDP (User Datagram Protocol) 
+ *    is connectionless and does not provide guarantees
  *    about packet delivery or order. This makes UDP faster and more suitable for applications where
  *    speed is more important than reliability. UDP is commonly used in real-time applications such as
  *    online gaming or video streaming, where a small amount of data loss is acceptable but low latency
