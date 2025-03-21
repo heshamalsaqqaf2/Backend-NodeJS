90c3057441d20124b1a104e3c211852f11d0c489
heshamalsaqqaf2 <heshamdev2@gmail.com>
2025-03-21 22:50:07 +0300

    Networking Protocols And Web Technologies.

---
+++ 04_Networking Protocols/README.js
@@ -0,0 +1,20 @@
+/** 
+ * @description
+ * 
+ *?   Networking Protocols and Web Technologies
+ *      Understand the rules and standards of networking protocols that enable communication
+ *      between devices in web development. This course covers HTTP versions (1.1, 2, and 3),
+ *      WebSockets for real-time communication, and compares TCP and UDP protocols. You'll also 
+ *      explore SSL, TLS, and HTTPS for secure communication, and learn the principles
+ *      of REST architecture for API design.
+ * 
+ *?   Content Course [4]
+ **      1- HTTP Protocol Versions
+ **      2- WebSockets
+ **      3- TCP vs UDP
+ **      4- SSL and TLS
+ **      5- SSL and TLS
+ **      6- HTTPS
+ **      7- REST, URLs, and DNS
+ **      8- Networking Protocols and Web Technologies Quiz
+*/
